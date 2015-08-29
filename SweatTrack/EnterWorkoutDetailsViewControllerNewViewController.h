f6aac39fedfba06ff0084b479aa961de46985388
Anand Kumar <write2anand.kumar@gmail.com>
2015-08-29 17:17:08 -0400

    WIP - 2015 08 29.

---
+++ SweatTrack/EnterWorkoutDetailsViewControllerNewViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterWorkoutDetailsViewControllerNewViewController.h
+//  SweatTrack
+//
+//  Created by Anand Kumar 5 on 8/27/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterWorkoutDetailsViewControllerNewViewController : UIViewController
+
+@end
---
+++ SweatTrack/EnterWorkoutDetailsViewControllerNewViewController.m
@@ -0,0 +1,39 @@
+//
+//  EnterWorkoutDetailsViewControllerNewViewController.m
+//  SweatTrack
+//
+//  Created by Anand Kumar 5 on 8/27/15.
+//
+//
+
+#import "EnterWorkoutDetailsViewControllerNewViewController.h"
+
+@interface EnterWorkoutDetailsViewControllerNewViewController ()
+
+@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation EnterWorkoutDetailsViewControllerNewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*r
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
